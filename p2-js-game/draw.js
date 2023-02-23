fd8e477a205fcc12bf336b76a547149519635198
reneetanoco <renee.tanoco@gmail.com>
2023-02-23 16:50:17 +0800

    feat(p2-js-game): added js files

---
+++ p2-js-game/draw.js
@@ -0,0 +1,94 @@
+function drawNet() 
+{
+    for (var i = 0; i < canvas.height; i += 40) 
+    {
+        colorRect(canvas.width / 2, i, 2, 20, 'white'); //the is divided by 2 to center the net. [i] is the the +=40 the space between the line. // 2 is the thickness. 20 is the height of each line
+
+    }
+
+}
+
+function draw() {
+    let scoreCpu = cpuScore.toString();
+    let scorePlayer = playerScore.toString();
+
+    colorRect(0, 0, canvas.width, canvas.height, 'black');
+
+    if (startGameScreen) 
+    {
+        winScreen = true;
+
+    }
+
+
+
+    if (winScreen) {
+        canvasContext.fillStyle = 'white';
+
+        if (cpuScore >= winScore) 
+        {
+            canvasContext.fillText("You failed humanity", 296, 200);
+            canvasContext.fillText('click to continue', 300, 500);
+
+
+        }
+
+        else if (playerScore >= winScore) 
+        {
+            canvasContext.fillText("You saved humanity", 299, 200);
+            canvasContext.fillText('click to continue', 300, 500);
+
+
+        }
+
+        else if (playerScore == 0 && cpuScore == 0) 
+        {
+
+            //I wanted to add line break but html canvas doesn't have that feature. 
+            let txt = '                    GAME OF PONG\n\n\n\n\n\n\n\n\n\nDrag the mouse to control the left paddle and \ntry to win against the AI before they take over the world!\n\nPress ESC to reset game\nPress Spacebar to stop the game';
+            let array = txt.split('\n'); // convert string to array and separate by /n 
+
+
+            let lineH = 15; // line height 
+
+
+            for (var i = 0; i < array.length; i++) // iterated each array in order to print on the screen
+            {
+                canvasContext.fillText(array[i], 100, 150 + (i * lineH)); //  i*lineH is the most important because it will seperate the arrays vertically
+            }
+
+        }
+
+
+        return;
+    }
+
+    drawCircle(ballX, ballY, 10, 'grey');
+
+    drawNet();
+
+
+    colorRect(0, paddleOneY, PADDLE_WIDTH, PADDLE_HEIGHT, 'white'); // player 1(left)
+    colorRect(canvas.width - PADDLE_WIDTH, paddleTwoY, PADDLE_WIDTH, PADDLE_HEIGHT, 'white'); // computer (right)
+
+    canvasContext.fillText(scorePlayer, 100, 100);
+
+    canvasContext.fillText(scoreCpu, canvas.width - 100, 100);
+
+
+}
+
+
+
+function drawCircle(x, y, radius, drawColor) {
+    canvasContext.fillStyle = drawColor; //ball
+    canvasContext.beginPath(); // define the shape
+    canvasContext.arc(x, y, 10, 0, Math.PI * 2, true); // (posX. posY, radius, angles, circumference, clockewise or counter)
+    canvasContext.fill();
+
+}
+
+function colorRect(leftX, topY, width, height, drawColor) {
+    canvasContext.fillStyle = drawColor;
+    canvasContext.fillRect(leftX, topY, width, height);
+}
---
+++ p2-js-game/move.js
@@ -0,0 +1,126 @@
+function calcMousePos(evt) // function that will execute everytime the mouse moves
+{
+    let rect = canvas.getBoundingClientRect(); // returns the size of the canvas 
+    let root = document.documentElement;
+    var mouseX = evt.clientX - rect.left - root.scrollLeft; // this will calculate where the playable area is in the web page.
+    var mouseY = evt.clientY - rect.top - root.scrollTop;
+    return {
+        x: mouseX,
+        y: mouseY
+    };
+}
+
+function mouseClick(evt) 
+{
+    if (winScreen) {
+        playerScore = 0;
+        cpuScore = 0;
+        winScreen = false;
+    }
+
+}
+
+function startGame(evt) // start
+{
+    if (startGameScreen) 
+    {
+        startGameScreen = false;
+    }
+
+}
+
+function resetGame(evt) 
+{
+    ballReset();
+    playerScore = 0;
+    cpuScore = 0;
+}
+
+function stopGame() {
+    startGameScreen = true;
+    resetGame();
+
+
+}
+
+
+
+
+function ballReset() // this will make the ball reset in the middle whenever a player scores
+{
+
+    if (cpuScore >= winScore || playerScore >= winScore) {
+        winScreen = true;
+    }
+    ballSpeedX = -ballSpeedX;
+    ballX = canvas.width / 2
+    ballY = canvas.height / 2
+
+}
+
+function cpuMove() //movement for cpu player
+{
+    let paddleTwoYMid = paddleTwoY + (PADDLE_HEIGHT / 2) // make the center part of the paddle the start point
+    if (paddleTwoYMid < ballY - 30) // the paddles stops moving if the direction of the ball is 30 pixels above the center
+    {
+        paddleTwoY += 6;
+    }
+
+    else if (paddleTwoYMid > ballY + 30) {
+        paddleTwoY -= 6;
+    }
+
+
+}
+
+function move() 
+{
+
+
+    if (winScreen) {
+        return;
+    }
+    cpuMove();
+    ballX += ballSpeedX; //makign the ball move
+    ballY += ballSpeedY;
+
+    if (ballX < 0) // collision detection for the paddle 
+    {
+        if (ballY > paddleOneY && ballY < paddleOneY + PADDLE_HEIGHT) // if the ball hit the paddle for paddleOne(player/left paddle) 
+        {
+            ballSpeedX = -ballSpeedX // bounce off the paddle 
+            let deltaY = ballY - (paddleOneY + PADDLE_HEIGHT / 2);// the speed of the ball will now depend on the angle and distance of the center of the paddle.
+            ballSpeedY = deltaY * 0.35;
+        }
+        else  // else not, reset the ball in the middle and add score to cpu
+        {
+            cpuScore++;
+            ballReset();
+        }
+
+    }
+
+    if (ballX > canvas.width) // collision detection the paddleTwo(CPU/right paddle)
+    {
+        if (ballY > paddleTwoY && ballY < paddleTwoY + PADDLE_HEIGHT) {
+            ballSpeedX = -ballSpeedX;
+            let deltaY = ballY - (paddleTwoY + PADDLE_HEIGHT / 2);
+            ballSpeedY = deltaY * 0.35;
+        }
+        else {
+            playerScore++;
+            ballReset();
+        }
+    }
+
+
+    if (ballY < 0) // maintain speed after bouncing off vertically
+    {
+        ballSpeedY = -ballSpeedY;
+    }
+
+    if (ballY > canvas.height) //bounces off  y-axis
+    {
+        ballSpeedY = -ballSpeedY;
+    }
+}
